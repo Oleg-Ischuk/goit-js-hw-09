00476ec515794cd989d3a9eb5f2b9754b25eec5b
Oleg_Ischuk <ipz231_ios@student.ztu.edu.ua>
2024-12-26 19:39:36 +0200

    HomeTask 9

---
+++ src/js/2-form.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('.feedback-form');
+const input = form.elements.email;
+const textarea = form.elements.message;
+const STORAGE_KEY = 'feedback-form-state';
+
+populateForm();
+
+form.addEventListener('input', getFormInformation);
+function getFormInformation() {
+  const formObj = {
+    email: input.value.trim(),
+    message: textarea.value.trim(),
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formObj));
+}
+
+function populateForm() {
+  const savedFormInfo = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  input.value = savedFormInfo.email ?? '';
+  if (savedFormInfo) {
+    textarea.value = savedFormInfo.message ?? '';
+  }
+}
+
+form.addEventListener('submit', sentInformation);
+function sentInformation(event) {
+  event.preventDefault();
+  const savedFormInfo = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+  if (savedFormInfo.email && savedFormInfo.message) {
+    console.log(savedFormInfo);
+  }
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
